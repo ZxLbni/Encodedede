fe3fcd2e12370f8f54fa6559e557fd85c42421ad
Mx Nitro <95213910+Devilharsha@users.noreply.github.com>
2024-04-30 12:18:22 +0530

    Update __init__.py
    Signed-off-by: Mx Nitro  <95213910+Devilharsha@users.noreply.github.com>

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -6,7 +6,7 @@ if os.path.exists('error.log'):
         
 #<-----------LOGGING------------>
 logging.basicConfig(level=logging.INFO, filename='error.log')
-LOG = logging.getLogger("Bot by @soheru")
+LOG = logging.getLogger("Bot by @MXnitro")
 LOG.setLevel(level=logging.INFO)
 #<-----------Variables-------------->
 LOG.info('❤️ Checking Bot Variables.....')
@@ -17,7 +17,7 @@ APP_HASH = os.environ.get('APP_HASH', 'your api id')# Telgram App hash
 OWNER_ID = int(os.environ.get('OWNER_ID', 953362604))
 MONGO_DB = os.environ.get("MONGO_DB", 'your mongodb') #MONGO DB FOR ANIME DATA
 FILES_CHANNEL = os.environ.get("FILES_CHANNEL", -100456789013)    # Must Fill This ,Add Bot As Admin In Log Channel
-BOT_NAME = os.environ.get('BOT_NAME', 'Soheru')
+BOT_NAME = os.environ.get('BOT_NAME', 'mxnitro')
 #<-----------Variables For 4GB Support (Optional)-------------->
 SESSION_STRING = os.environ.get("SESSION_STRING",'None')  #Replace None With String Session   Your String Session Account Must Be Present In Log Channel
 ubot = None  # Don't Touch This
@@ -25,7 +25,7 @@ ubot = None  # Don't Touch This
 if BOT_TOKEN is not None:
     try:
         encoder  = Client('AutoEncoder', api_id=API_ID, api_hash=APP_HASH, bot_token=BOT_TOKEN, plugins=dict(root="Bot/plugins"))
-        LOG.info('❤️ Bot Connected Created By Github @soheru || Telegram @sohailkhan_indianime ')
+        LOG.info('❤️ Bot Connected Created By Github @Devilharsha || Telegram @Mxnitro ')
     except Exception as e:
         LOG.warn(f'😞 Error While Connecting To Bot\nCheck Errors: {e}')
         sys.exit()       
